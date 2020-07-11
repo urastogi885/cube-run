3206083d553b81033ea42087a0a915f70dc89a5a
Umang Rastogi <umang.rastogi96@gmail.com>
2020-07-10 20:39:01 -0400

    [FollowPlayer.cs] Make the camera follow the player

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    // Instantiate variable to get pose of the player
+    public Transform playerPose;
+    public Vector3 camOffset;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = playerPose.position + camOffset;
+    }
+}
