578989f8e90174a6fdd40fddc7898f1e083a4384
Umang Rastogi <umang.rastogi96@gmail.com>
2020-07-10 20:37:32 -0400

    [PlayerCollision.cs] Add check for player's collision
    End game upon collision

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    // Instantiate an object of the player-movement class
+    public PlayerMovement playerMovement;
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            // Stop player movement upon collision with an obstacle
+            playerMovement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
