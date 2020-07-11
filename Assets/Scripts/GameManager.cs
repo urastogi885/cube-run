9b7bde19cfa2eb99f5203af4d9f95669079bb9ef
Umang Rastogi <umang.rastogi96@gmail.com>
2020-07-10 20:52:08 -0400

    [GameManager.cs] Define class to manage the game
    - End or restart game

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private bool gameEnded = false;
+    public float restartDelay = 1f;
+    public GameObject completeLevelUI;
+    // Method to end the game
+    public void EndGame()
+    {
+        if (gameEnded == false)
+        {
+            gameEnded = true;
+            Debug.Log("GAME OVER!");
+            Invoke("Restart", restartDelay);
+        }
+
+    }
+
+    private void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void CompleteLevel()
+    {
+        completeLevelUI.SetActive(true);
+    }
+}
