744c582e701450e26ba315179a1cc39aa1359242
Umang Rastogi <umang.rastogi96@gmail.com>
2020-07-10 20:47:21 -0400

    [EndTrigger.cs] Add object to trigger end of a level

---
+++ Assets/Scripts/EndTrigger.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class EndTrigger : MonoBehaviour
+{
+    public GameManager gameManager;
+    // Method to trigger end of a level in the game
+    private void OnTriggerEnter()
+    {
+        Debug.Log("Level Completed!");
+        gameManager.CompleteLevel();
+    }
+}
