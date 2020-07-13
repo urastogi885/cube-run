77939e95b98dc0213e03410129959ce3acce1409
Umang Rastogi <umang.rastogi96@gmail.com>
2020-07-12 23:33:05 -0400

    [Menu.cs] Add method in the menu class to exit the game
    Method is called when the button is clicked

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -3,9 +3,24 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    // Method to start the game
     public void StartGame()
     {
         Debug.Log("Game started!");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    // Method to go to control settings scene
+    public void ControlSettings()
+    {
+        Debug.Log("Switched to control settings scene");
+        // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    // Method to exit the game
+    public void QuitGame()
+    {
+        Debug.Log("Player has quit the game!");
+        Application.Quit();
     }
 }
