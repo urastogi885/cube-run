5675c135d73bf71ada29a35bc40f91c7324e2aeb
Umang Rastogi <umang.rastogi96@gmail.com>
2020-07-13 00:08:38 -0400

    [Controls.cs] Add method to return to main menu
    Method is callable on "Back to Main Menu" button-click

---
+++ Assets/Scripts/Controls.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Controls : MonoBehaviour
+{
+    public void ReturnToMainMenu()
+    {
+        Debug.Log("Return back to the main menu");
+        SceneManager.LoadScene("Menu");
+    }
+}
